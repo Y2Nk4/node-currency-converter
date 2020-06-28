85670367cafb5540ef916a639e28dee8b11c01ae
Y2Nk4 <1147678303@qq.com>
2020-06-27 21:51:55 -0400

    update jest

--- test/module.test.js
+++ test/module.test.js
@@ -6,3 +6,8 @@ test('converter.convert should return a float value', async () => {
     expect(typeof await converter.convert('USD', 'CNY', 1))
         .toBe('number')
 })
+
+test('converter.convert should return a object if isRaw set to be true', async () => {
+    expect(typeof await converter.convert('USD', 'CNY', 1, true))
+        .toBe('object')
+})
